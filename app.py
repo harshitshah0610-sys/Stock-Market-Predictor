3e939599711361fefefc7ace49c497a1611da065
Harshit Shah <harshitshah0610-sys@github.com>
2026-04-23 15:30:35 +0530

    Fix app.py for production deployment: dynamic PORT, disable debug, add os import

--- app.py
+++ app.py
@@ -521,6 +521,9 @@ def get_indian_time():
     except:
         return datetime.now() + timedelta(hours=5, minutes=30)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
 
